943245d3d7d9fe4c25ea4294c305eec2bc46e004
Br1Filippi <bfilippi@usm.cl>
2024-08-29 19:27:56 -0400

    Fixed Menu
    Fixed Menu Inteface

--- resources/views/templates/master.blade.php
+++ resources/views/templates/master.blade.php
@@ -9,8 +9,8 @@
 </head>
 <body>
     {{-- Navbar --}}
-    <nav class="navbar navbar-expand-lg navbar-light flex-column vh-100 p-3" style="width: 250px; background-color: #005E90; ">
-
+    <nav class="navbar navbar-expand-lg navbar-light flex-column vh-100 p-3" style="width: 250px; background-color: #005E90;">
+        
         {{-- Logotipo --}}
         <a class="navbar-brand d-flex align-items-center mb-4" href="#">
             <img src="{{ asset('images/Logo_UTFSM.png') }}" alt="Logo UTFSM" style="max-width: 80px; height: auto;">
@@ -21,31 +21,49 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse flex-column d-flex" id="navbarNav">
+
+        <div class="collapse navbar-collapse flex-column d-flex justify-content-between h-100" id="navbarNav">
+
+            {{-- Menu de Navegacion  --}}
+            <ul class="navbar-nav flex-column w-100 list-group">
+
+                {{-- Empresas --}}
+                {{-- @if(Route::current()->getName()=='home.index') active @endif" --}}
+                <a class="list-group-item list-group-item-action d-flex align-items-center " href="">
+                    <span class="material-icons me-2 ">view_list</span>
+                    Ofertas
+                </a>
+                <a class="list-group-item list-group-item-action d-flex align-items-center" href="">
+                    <span class="material-icons me-2 ">groups</span>
+                    Practicantes
+                </a>
+                {{-- Empresas --}}
+
+
+            </ul>
+            {{-- Menu de Navegacion  --}}
+
+            {{-- Perfil  --}}
             <ul class="navbar-nav flex-column w-100">
                 <li class="nav-item">
-                    <a class="nav-link active text-white" aria-current="page" href="#">Inicio</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="#">Características</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="#">Precios</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="#">Acerca de</a>
-                </li>
-                <li class="nav-item mt-auto">
                     <hr class="dropdown-divider" />
-                    <a class="nav-link text-white d-flex align-items-center" href="#">
-                        <span class="material-icons me-2">person</span>
+                    <a class="nav-link text-white d-flex align-items-center fs-4 py-4" href="#">
+                        <span class="material-icons me-2 fs-3">person</span>
                         Perfil
                     </a>
                 </li>
             </ul>
+            {{-- /*Perfil  --}}
+
         </div>
     </nav>
     {{-- /*Navbar --}}
+
+    {{-- Pagina Principal --}}
+    <div class="p-3 pt-1">
+        @yield('contenido-principal')
+    </div>
+{{-- /Pagina Principal --}}
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
